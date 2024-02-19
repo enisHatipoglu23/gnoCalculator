a272b3406b00fd64c92a29404b73b24b2ef7d5cd
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2024-02-19 10:08:49 +0300

    Methods updated.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //importing the class and running the function.
         GradeCalculator gradeCalculator = new GradeCalculator();
         gradeCalculator.calc();
     }
